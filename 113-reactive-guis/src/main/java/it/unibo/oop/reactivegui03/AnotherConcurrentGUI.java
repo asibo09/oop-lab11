ad3716e941665840d7123a33b173bd9ae706f567
PietroSiboni <pietro.siboni3@studio.unibo.it>
2024-12-07 12:50:19 +0100

    implemented in ex 103 part reactivegui03 the class AnotherConcurrentGUI.java

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -1,9 +1,8 @@
 package it.unibo.oop.reactivegui03;
 
+
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.TimeUnit;
 
@@ -14,8 +13,99 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 /**
- * Third experiment with reactive gui.
+ * This is a first example on how to realize a reactive GUI.
+ * This shows an alternative solutions using lambdas
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class AnotherConcurrentGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private static final long WAITING_TIME = TimeUnit.SECONDS.toMillis(10);
+    private final JLabel display = new JLabel();
+    private final JButton stop = new JButton("stop");
+    private final JButton up = new JButton("up");
+    private final JButton down = new JButton("down");
+    private final Agent agent = new Agent();
+
+    /**
+     * Builds a new CGUI.
+     */
+    public AnotherConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(up);
+        panel.add(down);
+        panel.add(stop);
+        this.getContentPane().add(panel);
+        this.setVisible(true);
+        up.addActionListener((e) -> agent.countUp());
+        down.addActionListener((e) -> agent.countDown());
+        stop.addActionListener((e) -> {
+            agent.stopCounting();
+            up.setEnabled(false);
+            down.setEnabled(false);
+            stop.setEnabled(false);
+        });
+        new Thread(agent).start();
+        new Thread(() -> {
+            try {
+                Thread.sleep(WAITING_TIME);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            agent.stopCounting();
+            SwingUtilities.invokeLater(() -> {
+                stop.setEnabled(false);
+                up.setEnabled(false);
+                down.setEnabled(false);
+            });
+        }).start();        
+    }
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private class Agent implements Runnable, java.io.Serializable {
+
+        private static final long serialVersionUID = 1L;
+        private volatile boolean stop;
+        private volatile boolean up = true;
+        private int counter;
+
+        @Override
+        public void run() {
+            while (!this.stop) {
+                try {
+                    final var toDisplay = Integer.toString(counter);
+                    SwingUtilities.invokeAndWait(() -> display.setText(toDisplay));
+                    counter += up ? 1 : -1;
+                    Thread.sleep(100);
+                } catch (InterruptedException | InvocationTargetException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.stop = true;
+        }
+
+        public void countUp(){
+            this.up = true;
+        }
+
+        public void countDown() {
+            this.up = false;
+        }
+    }
 }
